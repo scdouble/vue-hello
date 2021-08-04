01d70d1cda92f77e9aa503f3c3e059f55c06f9f8
Tatsu Sei <doubledragontaz@gmail.com>
2021-08-04 22:18:57 +0900

    CountUp vuex getter

---
+++ 25_src_CountUp_vuex_getters/store/index.js
@@ -0,0 +1,61 @@
+import { createStore } from "vuex";
+
+//業務ロジックはactions
+const actions = {
+  //jia(){}
+  jia: function(context, value) {
+    //console.log("actionsの中のjia", context, value);
+    context.commit("JIA", value); //区別できるようにMutationは大文字にするのが習慣
+  },
+  jian: function(context, value) {
+    context.commit("JIAN", value);
+  },
+
+  jiaOdd: function(context, value) {
+    if (context.state.sum % 2) {
+      // demo1につなぐ
+      context.dispatch("demo1", value);
+    }
+  },
+  demo1(context, value) {
+    console.log("demo1のactionが始まった");
+    context.commit("JIA", value);
+  },
+
+  jiaWait: function(context, value) {
+    setTimeout(() => {
+      context.commit("JIA", value);
+    });
+  },
+};
+
+// データ(state)を変更するためのmutation
+const mutations = {
+  JIA: function(state, value) {
+    console.log("Mutationの中のJIA", state, value);
+    state.sum += value;
+  },
+  JIAN: function(state, value) {
+    console.log("Mutationの中のJIA", state, value);
+    state.sum -= value;
+  },
+};
+// データを保管する
+const state = {
+  sum: 0,
+};
+
+// gettersを作成。stateのデータを加工するため。
+const getters = {
+  bigSum: function(state) {
+    return state.sum * 10;
+  },
+};
+// Create a new store instance.
+
+export default createStore({
+  actions,
+  mutations,
+  state,
+  getters,
+});
