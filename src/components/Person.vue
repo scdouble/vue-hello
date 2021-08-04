95a30057b50f21a25185a4d768675aecc29e9894
Tatsu Sei <doubledragontaz@gmail.com>
2021-08-04 23:40:23 +0900

    CountUp_vuex_share_state

---
+++ src/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Person List</h1>
+    <h3 style="color: red">Countコンポーネントの合計は{{ sum }}</h3>
+    <input type="text" placeholder="Please input name" v-model="name" />
+    <button @click="add">追加</button>
+    <ul>
+      <li v-for="p in personList" :key="p.id">{{ p.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import { mapState } from "vuex";
+import { nanoid } from "nanoid";
+export default {
+  name: "Person",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    add() {
+      const personObj = {
+        id: nanoid(),
+        name: this.name,
+      };
+      this.$store.commit("ADD_PERSON", personObj);
+      this.name = "";
+    },
+  },
+  computed: {
+    personList() {
+      return this.$store.state.personList;
+    },
+    sum() {
+      return this.$store.state.sum;
+    },
+    // ...mapState(["personList"]),
+  },
+};
+</script>
+
+<style>
+</style>
